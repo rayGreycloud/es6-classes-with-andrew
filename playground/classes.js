23bb51fa0e9d955f5d9783e090589c55e4dd97b6
rayGreycloud <rayGreycloud@gmail.com>
2016-10-13 14:40:00 -0700

    Class extends demo

--- playground/classes.js
+++ playground/classes.js
@@ -4,10 +4,16 @@ class Person {
     this.age = age;
   }
   getGreeting() {
-    return `Hi, my name is ${this.name}.`;
+    return `Hello, I'm ${this.name}.`;
   }
   getDescription() {
     return `${this.name} is ${this.age} year(s) old.`;
+  }
+}
+
+class Child extends Person {
+  getGreeting() {
+    return `Hi, my name is ${this.name} and I like kitties.`;
   }
 }
 
@@ -15,6 +21,6 @@ var me = new Person('Darth', 49);
 console.log(me.getGreeting());
 console.log(me.getDescription());
 
-var myself = new Person();
+var myself = new Child();
 console.log(myself.getGreeting());
 console.log(myself.getDescription());
