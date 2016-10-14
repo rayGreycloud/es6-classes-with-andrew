d362294e2d2c7b25d1d8fecaa5290bd02c7cba0c
rayGreycloud <rayGreycloud@gmail.com>
2016-10-14 10:30:37 -0700

    Set initial state on both components

--- app/components/ComponentOne.jsx
+++ app/components/ComponentOne.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
 var ComponentOne = React.createClass({
+  getInitialState: function () {
+    return {
+      count: 0
+    };
+  },
   render: function () {
     return (
       <div>
         <h3>Component One using React.createClass</h3>
+        <p>Current count: {this.state.count}</p>
       </div>
     );
   }
--- app/components/ComponentTwo.jsx
+++ app/components/ComponentTwo.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
 class ComponentTwo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 9
+    };
+  }
   render() {
     return (
       <div>
         <h3>Component Two using React.Component</h3>
+        <p>Current count: {this.state.count}</p>
       </div>
     );
   }
