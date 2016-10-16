773de3352ed272c0548496ac7570a7178e55457e
rayGreycloud <rayGreycloud@gmail.com>
2016-10-16 07:13:29 -0700

    Add Higher Order Function examples

--- playground/higher-order-functions.js
+++ playground/higher-order-functions.js
@@ -5,16 +5,25 @@
 // 2. Takes a function as only argument
 // 3. Returns a function
 
-// Demo of access to Args
-function one(name, location) {
-  console.log(name, location);
+// Example
+var add = (a, b) => a + b;
+console.log(`Result is ${add(99, 1)}`);
+
+// HOF
+var callAndLog = (func) => {
+  return function () {
+    var res = func.apply(undefined, arguments);
+    console.log(`Result is ${res}`);
+    return res;
+  }
 };
 
-function two() {
-  console.log('Args', arguments);
-  one.apply(undefined, arguments);
+// Using HOF
+var addAndLog = callAndLog(add);
+addAndLog(199, 1);
 
-};
+// Challenge: square function that uses HOF
+var square = (c) => c * c;
 
-two('Fred', 'Bedrock');
-two('Barney', 'New Rock City');
+var squareAndLog = callAndLog(square);
+squareAndLog(25);
